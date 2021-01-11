69b4e4d057f788b132707654461552edf4bce47a
julianmadoz <64795467+julianmadoz@users.noreply.github.com>
2021-01-11 12:22:03 -0300

    Add files via upload

---
+++ Poemas.js
@@ -0,0 +1,147 @@
+    let imagesamount = 10
+    let active_images = 2
+    let imageSizeXY = 300
+    let img_t = 0
+    let img_am_text = 0
+    let img_close = 0
+
+
+    class post {
+    	constructor() {
+    		this.title = ''
+    		this.text = ''
+    		this.date = ''
+    	}
+    }
+
+
+    function setup() {
+
+    	post = new post()
+    	post.title = createInput( '' )
+    	post.date = createInput( '' )
+    	post.text = createElement( 'textarea', '' )
+    	post.text.attribute( "rows", "15" );
+    	post.text.attribute( "cols", "70" );
+    	cv = createCanvas( windowWidth, imageSizeXY )
+    	createButton( 'Save' ).mousePressed( btn_p )
+    	images = []
+    	createGR()
+
+
+    }
+
+    function draw() {
+    	clear_all()
+    	background( 220 )
+    	print_title_date()
+    	elaborate_text()
+    	print_closer( active_images - 1 )
+    	display_images()
+
+
+    }
+
+
+    function btn_p() {
+    	noLoop()
+    	for ( let i = 0; i < active_images; i++ ) {
+    		name = post.title.value() + str( i ) + '_' + floor( millis() ) + '.jpg'
+    		images[ i ].save( name )
+
+
+    	}
+    }
+
+    function createGR() {
+    	for ( let i = 0; i < imagesamount; i++ ) {
+    		images[ i ] = createGraphics( 1000, 1000 )
+
+    	}
+    }
+
+    function print_title_date() {
+    	images[ 0 ].textAlign( CENTER, CENTER )
+    	images[ 0 ].textFont( 'Rokkitt', 100 )
+    	images[ 0 ].textStyle( BOLD )
+    	images[ 0 ].fill( 'Black' )
+    	// images[ 0 ].text( post.title.value(), images[ 0 ].width / 2, images[ 0 ].height / 2 )
+    	images[ 0 ].text( post.title.value(), images[ 0 ].width / 10, images[ 0 ].height / 3, 8 * images[ 0 ].width / 10, images[ 0 ].height / 3 )
+    	images[ 0 ].textFont( 'Rokkitt', 50 )
+    	images[ 0 ].textStyle( NORMAL )
+    	images[ 0 ].fill( 'Grey' )
+    	images[ 0 ].text( post.date.value(), images[ 0 ].width / 2, 2 * images[ 0 ].height / 3 )
+
+    }
+
+    function print_closer( closer ) {
+    	images[ closer ].textAlign( CENTER, CENTER )
+    	images[ closer ].textFont( 'Rokkitt', 100 )
+    	images[ closer ].textStyle( BOLD )
+    	images[ closer ].fill( 'Black' )
+    	images[ closer ].text( '. A quien corresponda', images[ active_images ].width / 2, images[ active_images ].height / 2 )
+    	images[ closer ].textFont( 'Rokkitt', 50 )
+    	images[ closer ].textStyle( NORMAL )
+    	images[ closer ].fill( 'Grey' )
+    	images[ closer ].text( '01/09/97', images[ active_images ].width / 2, 2 * images[ active_images ].height / 3 )
+
+    }
+
+    function print_text( text, i ) {
+    	images[ i ].textFont( 'Rokkitt', 40 )
+    	images[ i ].textStyle( NORMAL )
+    	images[ i ].textAlign( RIGHT, CENTER )
+    	images[ i ].fill( 0, 0, 0, 76 )
+    	offset = 1
+    	images[ i ].text( text, 100 + offset, 200 + offset, 800, 600 )
+    	images[ i ].fill( 'Black' )
+    	images[ i ].text( text, 100, 200, 800, 600 )
+
+    }
+
+    function elaborate_text() {
+    	oText = post.text.value()
+    	text = splitTokens( oText, '|' )
+    	if ( text.length >= 8 ) {
+    		active_images = 10
+    		img_am_text = 8
+    	} else {
+    		img_am_text = text.length
+    		active_images = text.length + 2
+    	}
+
+    	for ( let i = 0; i < img_am_text; i++ ) {
+    		print_text( text[ i ], i + 1 )
+    	}
+
+    }
+
+    function display_images() {
+    	colsAM = floor( windowWidth / imageSizeXY )
+    	rowsAM = 1 + floor( active_images / colsAM )
+    	resizeCanvas( windowWidth, imageSizeXY * rowsAM )
+    	row = 0
+    	col = 0
+    	for ( let i = 0; i < active_images; i++ ) {
+
+    		if ( col == colsAM ) {
+
+    			col = 0
+    			row = row + 1
+
+    		}
+    		image( images[ i ], ( imageSizeXY + 1 ) * col, ( imageSizeXY + 1 ) * row, imageSizeXY, imageSizeXY, 0, 0, 1000, 1000 )
+    		col = col + 1
+    	}
+
+
+    }
+
+
+    function clear_all() {
+    	for ( let i = 0; i < imagesamount; i++ ) {
+    		images[ i ].clear()
+    		images[ i ].background( 100 )
+    	}
+
+    }
